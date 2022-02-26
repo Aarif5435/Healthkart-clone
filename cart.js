026ee0dac188fc4ef4963007ddf2113477e9a27e
Shawon1997 <16haw87@gmail.com>
2022-02-26 21:36:42 +0530

    upload
    cart page

---
+++ cart.js
@@ -0,0 +1,19 @@
+let cartitem = document.querySelector("#cartItem")
+let data = JSON.parse(localStorage.getItem("cartdata"))
+
+function display(data) {
+    cartitem.innerHTML = []
+    data.forEach(el => {
+        let div = document.createElement("div")
+        let image = document.createElement("img")
+        image.src = el.image;
+        let title = document.createElement("h4")
+        title.innerText = el.title
+        let price = document.createElement("p")
+        price.innerText = el.price;
+    });
+}
+
+function paymentfun() {
+    window.location.href = "./payment.html"
+}
